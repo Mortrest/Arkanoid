de30bc55087762b81ce87cefa138050709a2050b
Ali Falahati <ali80af@gmail.com>
2021-03-28 21:56:51 +0430

    Fixed bouncing, Added blocks

---
+++ src/Block.java
@@ -0,0 +1,11 @@
+public class Block {
+    int blockX;
+    int blockY;
+
+    Block(int x, int y){
+        this.blockX = x;
+        this.blockY = y;
+    }
+    public void checkBlock(){
+    }
+}
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.sql.Time;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
@@ -15,14 +16,15 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int DELAY = 75;
     final int shipX[] = new int[GAME_UNITS];
     final int shipY[] = new int[GAME_UNITS];
-    int ballX = UNIT_SIZE*3;
-    int ballY = SCREEN_WIDTH-UNIT_SIZE*2;
+    int ballX = UNIT_SIZE * 6;
+    int ballY = SCREEN_WIDTH - UNIT_SIZE * 2;
     int shipSize = 4;
     int wallsBroken;
     int collisions = 0;
     int gradientX = 1;
     int gradientY = -1;
     int BALL_UNIT = 30;
+    LinkedList<Block> blocks = new LinkedList<>();
     boolean running = false;
     char direction = 'R';
     Timer timer;
@@ -40,10 +42,10 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void startGame() {
-        newRowWalls();
         running = true;
         timer = new Timer(DELAY, this);
         timer.start();
+        newRowBlocks();
     }
 
     public void paintComponent(Graphics g) {
@@ -51,13 +53,8 @@ public class GamePanel extends JPanel implements ActionListener {
         draw(g);
     }
 
-    public void newRowWalls() {
-        for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
-
-        }
-    }
-
     public void draw(Graphics g) {
+
         for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
@@ -65,34 +62,70 @@ public class GamePanel extends JPanel implements ActionListener {
 
         for (int i = 0; i < shipSize; i++) {
             g.setColor(Color.BLUE);
-            g.fillRect(shipX[i]+UNIT_SIZE, SCREEN_WIDTH-UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+            g.fillRect(shipX[i] + UNIT_SIZE, SCREEN_WIDTH - UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
         }
+        if (running) {
+            g.setColor(Color.red);
+            for (Block block : blocks){
+                g.fillRect(block.blockX,0,40,25);
+            }
 
-        g.setColor(Color.white);
-        g.fillOval(ballX,ballY,UNIT_SIZE,UNIT_SIZE);
+            g.drawLine(0, SCREEN_WIDTH - UNIT_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH - UNIT_SIZE);
+            g.setColor(Color.white);
+            g.fillOval(ballX, ballY, UNIT_SIZE, UNIT_SIZE);
+        }
     }
 
-    public void moveBall(){
-        switch (collisions % 4) {
-            case 0 -> {
-                gradientX = 1;
-                gradientY = -1;
-            }
-            case 1 -> {
-                gradientX = -1;
-                gradientY = -1;
-            }
-            case 2 -> {
-                gradientX = -1;
+
+    public void newRowBlocks(){
+        for (int i = 0; i < SCREEN_WIDTH/UNIT_SIZE/2; i++) {
+            blocks.add(new Block(i*UNIT_SIZE*2,0));
+        }
+    }
+
+    public void moveBall() {
+        ballX += gradientX * BALL_UNIT;
+        ballY += gradientY * BALL_UNIT;
+        int max = 0;
+        int min = 600;
+        for (int i = 0; i < shipSize; i++) {
+            max = Math.max(shipX[i],max);
+            min = Math.min(shipX[i],min);
+        }
+        if ( min-20 <= ballX && ballX <= max+60 && ballY == SCREEN_HEIGHT - UNIT_SIZE * 2){
+            gradientY = -1;
+        }
+        int i = 0;
+        for (Block block : blocks){
+            System.out.println(block.blockX - 20 + " " + ballX);
+            if (40 == ballY && block.blockX - 20 <= ballX && block.blockX + 20 >= ballX){
                 gradientY = 1;
-            }
-            case 3 -> {
-                gradientX = 1;
-                gradientY = 1;
+                blocks.remove(block);
+                System.out.println(i);
+                break;
             }
         }
-        ballX += gradientX*BALL_UNIT;
-        ballY += gradientY*BALL_UNIT;
+
+        if (ballX >= SCREEN_WIDTH - UNIT_SIZE - 15) {
+            gradientX = -1;
+        }
+        if (ballX <= UNIT_SIZE) {
+            gradientX = 1;
+        }
+        if (ballY <= 20) {
+            gradientY = 1;
+        }
+        if (ballY >= SCREEN_HEIGHT) {
+            System.exit(0);
+        }
+    }
+
+    public void createBlock(Graphics g){
+        for (int i = 0; i < SCREEN_WIDTH/UNIT_SIZE/2; i++) {
+            g.setColor(Color.red);
+            blocks.add(new Block(i*UNIT_SIZE*2,0));
+            g.fillRect(i*UNIT_SIZE*2,0,40,25);
+        }
     }
 
     public void move(int type) {
@@ -108,28 +141,10 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
 
-    public void ballCollision(){
-        if (collisions>= 1){
-            for (int i = 0; i < shipSize; i++) {
-                if (shipX[i] == ballX && SCREEN_WIDTH-UNIT_SIZE == ballY){
-                    System.out.println("oh");
-                    collisions++;
-                }
-            }
-        }
-        if (ballX >= SCREEN_WIDTH || ballX <= UNIT_SIZE){
-            collisions++;
-        }
-        if (ballY <= UNIT_SIZE || ballY >= SCREEN_HEIGHT - UNIT_SIZE*2){
-            collisions++;
-        }
-
-
-    }
 
     public boolean checkCollisions1() {
         for (int i = 0; i < shipSize; i++) {
-            if (shipX[i] == SCREEN_WIDTH-UNIT_SIZE*2) {
+            if (shipX[i] == SCREEN_WIDTH - UNIT_SIZE * 2) {
                 return false;
             }
         }
@@ -153,11 +168,11 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKey extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            switch(e.getKeyCode()) {
+            switch (e.getKeyCode()) {
                 case KeyEvent.VK_LEFT:
-                    if (checkCollisions2() ) {
+                    if (checkCollisions2()) {
                         if (direction == 'R') {
-                            for (int i = 0; i < shipSize-2; i++) {
+                            for (int i = 0; i < shipSize - 2; i++) {
                                 move(2);
                             }
                         }
@@ -168,9 +183,10 @@ public class GamePanel extends JPanel implements ActionListener {
                 case KeyEvent.VK_RIGHT:
                     if (checkCollisions1()) {
                         if (direction == 'L') {
-                            for (int i = 0; i < shipSize-2; i++) {
+                            for (int i = 0; i < shipSize - 2; i++) {
                                 move(1);
-                            }                        }
+                            }
+                        }
                         direction = 'R';
                         move(1);
                     }
@@ -184,7 +200,6 @@ public class GamePanel extends JPanel implements ActionListener {
 
         if (running) {
             moveBall();
-            ballCollision();
         }
         repaint();
     }
